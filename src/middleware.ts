f427b935969fb96bab19c5693fee12ee11ca8ad0
Andrew-Sem <avvyayvayva@gmail.com>
2024-11-21 19:22:02 +0300

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import logger from './logger';
 
 export async function middleware(req: NextRequest) {
-    logger.info("\u001b[32m\u001b[1m✓\u001b[22m\u001b[39m Compiled in 51ms (76 modules)");
+    logger.info(req.nextUrl, "incoming request");
 
     return NextResponse.next();
 }
