d197e4567398a6ecc37d86ecc901d839bc321dc5
Andrew-Sem <avvyayvayva@gmail.com>
2024-11-21 19:20:28 +0300

    test

--- src/logger.ts
+++ src/logger.ts
@@ -2,12 +2,9 @@ import pino from 'pino';
 
 const logger = pino({
     transport: {
-      target: 'pino-pretty', // Для форматирования логов в читаемый вид
+      target: 'pino-pretty',
       options: {
-        colorize: true, // Цветные логи
-        levelFirst: true,
-        translateTime: 'yyyy-mm-dd HH:MM:ss',
-        ignore: 'pid,hostname', // Уберите ненужные поля
+        colorize: true,
       },
     },
   });
