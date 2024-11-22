af678b6ee944435c2c6d523a2da10fd0b3e34634
Andrew-Sem <avvyayvayva@gmail.com>
2024-11-22 06:46:28 +0300

    test

--- next-logger.config.js
+++ next-logger.config.js
@@ -1,12 +1,31 @@
-const pino = require('pino')
+const pino = require('pino');
 
 const logger = defaultConfig =>
   pino({
     ...defaultConfig,
     messageKey: 'message',
-    mixin: () => ({ name: 'custom-pino-instance' }),
-  })
+    mixin: (context) => {
+      const additionalAttributes = context ? context : {};
+      return {
+        name: 'custom-pino-instance',
+        ...additionalAttributes,
+      };
+    },
+    formatters: {
+      log: (object) => {
+        if (object.message && typeof object.message === 'string') {
+          try {
+            const parsedMessage = JSON.parse(object.message);
+            return { ...parsedMessage };
+          } catch (err) {
+            return object;
+          }
+        }
+        return object;
+      },
+    },
+  });
 
 module.exports = {
   logger,
-}
+};
