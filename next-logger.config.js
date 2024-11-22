70f808fcb18adb490a59754cdc233b3259089d3b
Andrew-Sem <avvyayvayva@gmail.com>
2024-11-22 10:02:40 +0300

    pretty

--- next-logger.config.js
+++ next-logger.config.js
@@ -1,60 +1,74 @@
-const { jsonrepair } = require('jsonrepair');
-const pino = require('pino');
+const { jsonrepair } = require("jsonrepair");
+const pino = require("pino");
+const pinoPretty = require("pino-pretty");
 
-const logger = (defaultConfig) =>
-  pino({
-    ...defaultConfig,
-    hooks: {
-      logMethod(inputArgs, method) {
-        if (inputArgs.length === 1 && typeof inputArgs[0] === 'string') {
-          const arg = inputArgs[0].trim();
-          try {
-            const jsonString = arg.replace(/'/g, '"');
-            const obj = JSON.parse(jsonString);
+const logger = (defaultConfig) => {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  return pino(
+    {
+      ...defaultConfig,
+      hooks: {
+        logMethod(inputArgs, method) {
+          if (inputArgs.length === 1 && typeof inputArgs[0] === "string") {
+            const arg = inputArgs[0].trim();
+            try {
+              const jsonString = arg.replace(/'/g, '"');
+              const obj = JSON.parse(jsonString);
 
-            let msg;
-            if (obj.hasOwnProperty('msg')) {
-              msg = obj.msg;
-              delete obj.msg;
-            }
+              let msg;
+              if (obj.hasOwnProperty("msg")) {
+                msg = obj.msg;
+                delete obj.msg;
+              }
 
-            if (msg) {
-              // Only pass 'msg' if it's not empty
-              return method.apply(this, [obj, msg]);
-            } else {
-              return method.apply(this, [obj]);
-            }
-          } catch (e) {
-            // Attempt to parse a message and a JSON object from the string
-            const regex = /^(.*?)(\{.*\})$/s;
-            const match = arg.match(regex);
-            if (match) {
-              const msg = match[1].trim();
-              const jsonStr = match[2];
-              try {
-                const obj = JSON.parse(jsonrepair(jsonStr.replace(/'/g, '"')));
-                if (msg) {
-                  return method.apply(this, [obj, msg]);
-                } else {
-                  return method.apply(this, [obj]);
+              if (msg) {
+                // Only pass 'msg' if it's not empty
+                return method.apply(this, [obj, msg]);
+              } else {
+                return method.apply(this, [obj]);
+              }
+            } catch (e) {
+              // Attempt to parse a message and a JSON object from the string
+              const regex = /^(.*?)(\{.*\})$/s;
+              const match = arg.match(regex);
+              if (match) {
+                const msg = match[1].trim();
+                const jsonStr = match[2];
+                try {
+                  const obj = JSON.parse(
+                    jsonrepair(jsonStr.replace(/'/g, '"'))
+                  );
+                  if (msg) {
+                    return method.apply(this, [obj, msg]);
+                  } else {
+                    return method.apply(this, [obj]);
+                  }
+                } catch (e2) {
+                  // Parsing failed; proceed to default behavior
                 }
-              } catch (e2) {
-                // Parsing failed; proceed to default behavior
               }
             }
           }
-        }
 
-        // Default behavior as per Pino documentation
-        if (inputArgs.length >= 2) {
-          const arg1 = inputArgs.shift();
-          const arg2 = inputArgs.shift();
-          return method.apply(this, [arg2, arg1, ...inputArgs]);
-        }
-        return method.apply(this, inputArgs);
+          // Default behavior as per Pino documentation
+          if (inputArgs.length >= 2) {
+            const arg1 = inputArgs.shift();
+            const arg2 = inputArgs.shift();
+            return method.apply(this, [arg2, arg1, ...inputArgs]);
+          }
+          return method.apply(this, inputArgs);
+        },
       },
     },
-  });
+    isDevelopment
+      ? pinoPretty({
+          colorize: true, // Enable colorized output
+          translateTime: "yyyy-mm-dd HH:MM:ss", // Human-readable time
+          ignore: "pid,hostname", // Ignore pid and hostname in logs
+        })
+      : undefined // Default behavior in production
+  );
+};
 
 module.exports = {
   logger,
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 function log(obj: Object, msg: string) {
-    console.log({ ...obj, msg });
-  }
-  
+  console.log({ ...obj, msg });
+}
 
 export async function middleware(req: NextRequest) {
-  log({ 
-    method: req.method, 
-    url: req.nextUrl.href, 
-    body: req.body, 
-    headers: Object.fromEntries(req.headers.entries()), 
-    cookies: req.cookies.getAll()
-  }, `${req.method} ${req.nextUrl.pathname}`);
-  // log({ losos: "karas" }, `${req.method} ${req.nextUrl.pathname}`);
+  log(
+    {
+      body: req.body,
+      headers: Object.fromEntries(req.headers.entries()),
+      cookies: Object.fromEntries(
+        req.cookies.getAll().map((c) => [c.name, c.value])
+      ),
+    },
+    `${req.method} ${req.nextUrl.pathname}`
+  );
 
   return NextResponse.next();
 }
