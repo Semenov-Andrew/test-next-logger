5b28bcd9c47b3569baf2a4e825dd81e5b8caa6f9
Andrew-Sem <avvyayvayva@gmail.com>
2024-11-22 05:16:17 +0300

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 import logger from './logger';
 
 export async function middleware(req: NextRequest) {
-    logger.info(Object.fromEntries(Object.entries(req.nextUrl)), "incoming request");
+    logger.info('handled request', {
+        request: {
+            url: req.nextUrl.href,
+            method: req.method,
+        },
+        response: {
+            status: 200,
+        },
+        duration: performance.now(),
+    });
 
     return NextResponse.next();
 }
